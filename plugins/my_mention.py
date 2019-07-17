dcc1b453ebbc6f43352b06a2fec15be4bdbcbb1c
wentmomori <wentmani@gmail.com>
2019-07-17 22:23:09 +0900

    initial commit

---
+++ plugins/my_mention.py
@@ -0,0 +1,64 @@
+# coding: utf-8
+import time
+import threading
+import math
+
+from time import sleep
+from slackbot.bot import respond_to     # @botname: で反応するデコーダ
+from slackbot.bot import listen_to      # チャネル内発言で反応するデコーダ
+from slackbot.bot import default_reply  # 該当する応答がない場合に反応するデコーダ
+
+# @respond_to('string')     bot宛のメッセージ
+#                           stringは正規表現が可能 「r'string'」
+# @listen_to('string')      チャンネル内のbot宛以外の投稿
+#                           @botname: では反応しないことに注意
+#                           他の人へのメンションでは反応する
+#                           正規表現可能
+# @default_reply()          DEFAULT_REPLY と同じ働き
+#                           正規表現を指定すると、他のデコーダにヒットせず、
+#                           正規表現にマッチするときに反応
+#                           ・・・なのだが、正規表現を指定するとエラーになる？
+
+# message.reply('string')   @発言者名: string でメッセージを送信
+# message.send('string')    string を送信
+# message.react('icon_emoji')  発言者のメッセージにリアクション(スタンプ)する
+#                               文字列中に':'はいらない
+
+def hour_time(message,clocktime):
+    hour1 = math.floor(clocktime/60) #時間
+    if hour1>1:
+      subtract_time = clocktime*60-hour1*3600 #指定したminuteからhourを引く
+      for i in range(hour1):
+          sleep(hour1*3600)
+          message.reply('{}時間経過シマシタ'.format(i+1))
+      sleep(subtract_time)
+      message.reply('{}時間{}分経過シマシタ'.format(hour1,subtract_time))
+    else:
+      sleep(clocktime*60)
+      message.reply('{}分経過シマシタ'.format(clocktime))
+    
+
+
+
+@respond_to('ハロー')
+def mention_func(message):
+    message.reply('ハローワールド！') # メンション
+
+@respond_to('(.*)分タイマー')
+def mention_func2(message,arg1):
+    try:
+        time1 = int(arg1) #分
+        if time1>300:
+          message.reply('300分以上ノタイマーニハ対応シテイマセン')
+        elif time1<1:
+          message.reply('１分以上ニシテクダサイ')
+        else:
+          message.reply('{}分後オシラセシマス'.format(time1)) # メンション
+          hour_time(message,time1)
+    except ValueError:
+          message.reply('分ノ前ハ数値ノミ入力シテクダサイ')
+
+@listen_to('暇な人')
+def listen_func(message):
+    message.send('ワタシハ、テガアイテマスヨ！')      # ただの投稿
+    message.reply('ナニカシマショウカ？')                           # メンション
---
+++ slackbot_settings.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# botアカウントのトークンを指定
+API_TOKEN = "xoxb-648397064673-683278021026-TtQFXp5X1hIL7psZZO8CJIQj"
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "ナニヲイッテイルカ、ワカリマセン"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
